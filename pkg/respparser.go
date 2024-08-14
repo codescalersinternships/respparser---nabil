2c14a2d5537f9a040bb4cde14a51a0683f4fb72d
nabil salah <nabil.salah203@gmail.com>
2024-08-14 17:19:02 +0300

    feat: resp parser implementation and testing and documenting
    Signed-off-by: nabil salah <nabil.salah203@gmail.com>

--- pkg/respparser.go
+++ pkg/respparser.go
@@ -8,10 +8,9 @@ import (
 
 const crlfTerminator = "\r\n"
 
-
-func readSimpleString(idx *int, str *string)(string, error){
+func readSimpleString(idx *int, str *string) (string, error) {
 	out := ""
-	lim := strings.Index((*str)[*idx:],crlfTerminator)
+	lim := strings.Index((*str)[*idx:], crlfTerminator)
 	if lim == -1 {
 		return "", fmt.Errorf("terminator wasn't found")
 	}
@@ -21,9 +20,9 @@ func readSimpleString(idx *int, str *string)(string, error){
 	return out, nil
 }
 
-func readErrorMSG(idx *int, str *string)(error,error){
+func readErrorMSG(idx *int, str *string) (error, error) {
 	out := ""
-	lim := strings.Index((*str)[*idx:],crlfTerminator)
+	lim := strings.Index((*str)[*idx:], crlfTerminator)
 	if lim == -1 {
 		return nil, fmt.Errorf("terminator wasn't found")
 	}
@@ -33,144 +32,143 @@ func readErrorMSG(idx *int, str *string)(error,error){
 	return fmt.Errorf(out), nil
 }
 
-func readInteger(idx *int, str *string)(int64,error){
+func readInteger(idx *int, str *string) (int64, error) {
 	out := ""
-	lim := strings.Index((*str)[*idx:],crlfTerminator)
+	lim := strings.Index((*str)[*idx:], crlfTerminator)
 	if lim == -1 {
 		return -1, fmt.Errorf("terminator wasn't found")
 	}
 	lim = lim + (*idx)
 	out = (*str)[*idx:lim]
 	*idx = lim + 1
-	integer,err := strconv.ParseInt(out, 10, 64)
+	integer, err := strconv.ParseInt(out, 10, 64)
 	if err != nil {
 		return -1, err
 	}
 	return integer, nil
 }
 
-func readBulkString(idx *int, str *string)(interface{},error){
+func readBulkString(idx *int, str *string) (interface{}, error) {
 	out := ""
 	length, err := readInteger(idx, str)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 	if length == -1 {
-		return nil,nil
+		return nil, nil
 	}
-	*idx += 1
+	(*idx)++
 	lim := (*idx) + int(length)
 	if lim > len(*str) {
-		return "", fmt.Errorf("can't find string with the given length")
+		return "", fmt.Errorf("string lenght isn't enough")
 	}
 	out = (*str)[*idx:lim]
 	if out == "-1" {
 		return "", nil
 	}
-	*idx = lim +1
+	*idx = lim + 1
 	return out, nil
 }
 
-func readBoolean (idx *int, str *string)(bool,error){
+func readBoolean(idx *int, str *string) (bool, error) {
 	if (*idx) >= len(*str) {
 		return false, fmt.Errorf("string lenght isn't enough")
-	} 
+	}
 	if (*str)[(*idx)] == 't' {
 		*idx += 2
 		return true, nil
-	}else {
-		*idx += 2
-		return false, nil
 	}
+	*idx += 2
+	return false, nil
 }
 
-func readArray(idx *int, str *string)([]interface{},error){
+func readArray(idx *int, str *string) ([]interface{}, error) {
 	length, err := readInteger(idx, str)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	
+
 	if length == -1 {
 		return nil, nil
 	}
 
 	var out []interface{}
-	*idx += 1;
-	for i := 0; i < int(length) ; i++ {
+	(*idx)++
+	for i := 0; i < int(length); i++ {
 		var curDatatype rune = rune((*str)[*idx])
-		switch curDatatype{
+		switch curDatatype {
 		case rune('+'):
-			(*idx) += 1
-			simpleStr, err :=readSimpleString(idx, str)
+			(*idx)++
+			simpleStr, err := readSimpleString(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, simpleStr)
-		
+
 		case rune('-'):
-			(*idx) += 1
-			msg, err :=readErrorMSG(idx, str)
+			(*idx)++
+			msg, err := readErrorMSG(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, msg)
 
 		case rune(':'):
-			(*idx) += 1
-			integer, err :=readInteger(idx, str)
+			(*idx)++
+			integer, err := readInteger(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, integer)
 
 		case rune('$'):
-			(*idx) += 1
-			bulkString, err :=readBulkString(idx, str)
+			(*idx)++
+			bulkString, err := readBulkString(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, bulkString)
 
 		case rune('*'):
-			(*idx) += 1
-			array, err :=readArray(idx, str)
+			(*idx)++
+			array, err := readArray(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, array)
 
 		case rune('_'):
-			(*idx) += 1
+			(*idx)++
 			out = append(out, nil)
-			*idx+=1
+			(*idx)++
 
 		case rune('#'):
-			(*idx) += 1
-			boolean, err :=readBoolean(idx, str)
+			(*idx)++
+			boolean, err := readBoolean(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, boolean)
 
 		case rune(','):
-			(*idx) += 1
-			float, err :=readFloat(idx, str)
+			(*idx)++
+			float, err := readFloat(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, float)
 
 		case rune('!'):
-			(*idx) += 1
-			bulkErrorMSG, err :=readBulkError(idx, str)
+			(*idx)++
+			bulkErrorMSG, err := readBulkError(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, bulkErrorMSG)
-		
+
 		case rune('%'):
-			(*idx) += 1
-			respMap, err :=readMap(idx, str)
+			(*idx)++
+			respMap, err := readMap(idx, str)
 			if err != nil {
 				return nil, err
 			}
@@ -178,138 +176,138 @@ func readArray(idx *int, str *string)([]interface{},error){
 		default:
 			return nil, fmt.Errorf("unsupported datatype")
 		}
-		*idx += 1;
+		(*idx)++
 	}
 	return out, nil
 }
 
-func readFloat(idx *int, str *string)(float64,error){
+func readFloat(idx *int, str *string) (float64, error) {
 	out := ""
-	lim := strings.Index((*str)[*idx:],crlfTerminator)
+	lim := strings.Index((*str)[*idx:], crlfTerminator)
 	if lim == -1 {
 		return -1, fmt.Errorf("terminator wasn't found")
 	}
 	lim = lim + (*idx)
 	out = (*str)[*idx:lim]
 	*idx = lim + 1
-	integer,err := strconv.ParseFloat(out, 64)
+	integer, err := strconv.ParseFloat(out, 64)
 	if err != nil {
 		return -1, err
 	}
 	return integer, nil
 }
 
-func readBulkError(idx *int, str *string)(error,error){
+func readBulkError(idx *int, str *string) (error, error) {
 	out := ""
 	length, err := readInteger(idx, str)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	*idx += 1
+	(*idx)++
 	lim := (*idx) + int(length)
 	if lim > len(*str) {
-		return nil, fmt.Errorf("can't find string with the given length")
+		return nil, fmt.Errorf("string lenght isn't enough")
 	}
 	out = (*str)[*idx:lim]
 	if out == "-1" {
 		return nil, nil
 	}
-	*idx = lim +1
-	
+	*idx = lim + 1
+
 	return fmt.Errorf(out), nil
 }
 
-func readMap(idx *int, str *string) (map[interface{}]interface{}, error){
+func readMap(idx *int, str *string) (map[interface{}]interface{}, error) {
 
 	out := make(map[interface{}]interface{})
 	length, err := readInteger(idx, str)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	
+
 	if length == -1 {
 		return nil, nil
 	}
 
-	*idx += 1;
+	(*idx)++
 	var prev interface{}
-	for i := 0; i < int(length)*2 ; i++ {
+	for i := 0; i < int(length)*2; i++ {
 		var curDatatype rune = rune((*str)[*idx])
 
 		var val interface{}
-		switch curDatatype{
+		switch curDatatype {
 		case rune('+'):
-			(*idx) += 1
-			simpleStr, err :=readSimpleString(idx, str)
+			(*idx)++
+			simpleStr, err := readSimpleString(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = simpleStr
-		
+
 		case rune('-'):
-			(*idx) += 1
-			msg, err :=readErrorMSG(idx, str)
+			(*idx)++
+			msg, err := readErrorMSG(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = msg
 
 		case rune(':'):
-			(*idx) += 1
-			integer, err :=readInteger(idx, str)
+			(*idx)++
+			integer, err := readInteger(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = integer
 
 		case rune('$'):
-			(*idx) += 1
-			bulkString, err :=readBulkString(idx, str)
+			(*idx)++
+			bulkString, err := readBulkString(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = bulkString
 
 		case rune('*'):
-			(*idx) += 1
-			array, err :=readArray(idx, str)
+			(*idx)++
+			array, err := readArray(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = array
 
 		case rune('_'):
-			(*idx) += 1
+			(*idx)++
 			val = nil
-			*idx+=1
+			(*idx)++
 
 		case rune('#'):
-			(*idx) += 1
-			boolean, err :=readBoolean(idx, str)
+			(*idx)++
+			boolean, err := readBoolean(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = boolean
 
 		case rune(','):
-			(*idx) += 1
-			float, err :=readFloat(idx, str)
+			(*idx)++
+			float, err := readFloat(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = float
 
 		case rune('!'):
-			(*idx) += 1
-			bulkErrorMSG, err :=readBulkError(idx, str)
+			(*idx)++
+			bulkErrorMSG, err := readBulkError(idx, str)
 			if err != nil {
 				return nil, err
 			}
 			val = bulkErrorMSG
-		
+
 		case rune('%'):
-			(*idx) += 1
-			respMap, err :=readMap(idx, str)
+			(*idx)++
+			respMap, err := readMap(idx, str)
 			if err != nil {
 				return nil, err
 			}
@@ -317,7 +315,7 @@ func readMap(idx *int, str *string) (map[interface{}]interface{}, error){
 		default:
 			return nil, fmt.Errorf("unsupported datatype")
 		}
-		*idx += 1;
+		(*idx)++
 		if i%2 == 1 {
 			out[prev] = val
 		}
@@ -328,25 +326,24 @@ func readMap(idx *int, str *string) (map[interface{}]interface{}, error){
 
 }
 
-
-//Parser reads an resp string, returning an array of interfaces of items
-func Parser(str string) ([]interface{} ,error){
-	var out []interface{};
+// Parser reads an resp string, returning an array of interfaces of items
+func Parser(str string) ([]interface{}, error) {
+	var out []interface{}
 	var idx int = 0
-	for ;idx < len(str); idx++ {
+	for ; idx < len(str); idx++ {
 		var curDatatype rune = rune(str[idx])
-		switch curDatatype{
+		switch curDatatype {
 		case rune('+'):
 			idx++
-			simpleStr, err :=readSimpleString(&idx, &str)
+			simpleStr, err := readSimpleString(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, simpleStr)
-		
+
 		case rune('-'):
 			idx++
-			msg, err :=readErrorMSG(&idx, &str)
+			msg, err := readErrorMSG(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
@@ -354,7 +351,7 @@ func Parser(str string) ([]interface{} ,error){
 
 		case rune(':'):
 			idx++
-			integer, err :=readInteger(&idx, &str)
+			integer, err := readInteger(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
@@ -362,7 +359,7 @@ func Parser(str string) ([]interface{} ,error){
 
 		case rune('$'):
 			idx++
-			bulkString, err :=readBulkString(&idx, &str)
+			bulkString, err := readBulkString(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
@@ -370,7 +367,7 @@ func Parser(str string) ([]interface{} ,error){
 
 		case rune('*'):
 			idx++
-			array, err :=readArray(&idx, &str)
+			array, err := readArray(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
@@ -379,11 +376,11 @@ func Parser(str string) ([]interface{} ,error){
 		case rune('_'):
 			idx++
 			out = append(out, nil)
-			idx+=1
+			idx++
 
 		case rune('#'):
 			idx++
-			boolean, err :=readBoolean(&idx, &str)
+			boolean, err := readBoolean(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
@@ -391,7 +388,7 @@ func Parser(str string) ([]interface{} ,error){
 
 		case rune(','):
 			idx++
-			float, err :=readFloat(&idx, &str)
+			float, err := readFloat(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
@@ -399,15 +396,15 @@ func Parser(str string) ([]interface{} ,error){
 
 		case rune('!'):
 			idx++
-			bulkErrorMSG, err :=readBulkError(&idx, &str)
+			bulkErrorMSG, err := readBulkError(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
 			out = append(out, bulkErrorMSG)
-		
+
 		case rune('%'):
 			idx++
-			respMap, err :=readMap(&idx, &str)
+			respMap, err := readMap(&idx, &str)
 			if err != nil {
 				return nil, err
 			}
--- pkg/respparser_test.go
+++ pkg/respparser_test.go
@@ -7,16 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
-func TestParse(t *testing.T){
+func TestParse(t *testing.T) {
 	tests := []struct {
-		name     string
-		input    string
-		expected []interface{}
+		name          string
+		input         string
+		expected      []interface{}
 		expectedError bool
 	}{
 		{
-			name: "Simple String",
+			name:  "Simple String",
 			input: "+OK\r\n",
 			expected: []interface{}{
 				"OK",
@@ -24,7 +23,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Error Message",
+			name:  "Error Message",
 			input: "-Error message\r\n",
 			expected: []interface{}{
 				fmt.Errorf("Error message"),
@@ -32,7 +31,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Integer",
+			name:  "Integer",
 			input: ":1000\r\n",
 			expected: []interface{}{
 				int64(1000),
@@ -40,7 +39,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Bulk String",
+			name:  "Bulk String",
 			input: "$6\r\nfoobar\r\n",
 			expected: []interface{}{
 				"foobar",
@@ -48,7 +47,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Null Bulk String",
+			name:  "Null Bulk String",
 			input: "$-1\r\n",
 			expected: []interface{}{
 				nil,
@@ -56,7 +55,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Array of Bulk Strings",
+			name:  "Array of Bulk Strings",
 			input: "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
 			expected: []interface{}{
 				[]interface{}{"foo", "bar"},
@@ -64,7 +63,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Array of Integers",
+			name:  "Array of Integers",
 			input: "*3\r\n:1\r\n:2\r\n:3\r\n",
 			expected: []interface{}{
 				[]interface{}{int64(1), int64(2), int64(3)},
@@ -72,7 +71,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Null Element",
+			name:  "Null Element",
 			input: "_\r\n",
 			expected: []interface{}{
 				nil,
@@ -80,7 +79,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Boolean True",
+			name:  "Boolean True",
 			input: "#t\r\n",
 			expected: []interface{}{
 				true,
@@ -88,7 +87,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Boolean False",
+			name:  "Boolean False",
 			input: "#f\r\n",
 			expected: []interface{}{
 				false,
@@ -96,7 +95,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Floating Point Number",
+			name:  "Floating Point Number",
 			input: ",123.45\r\n",
 			expected: []interface{}{
 				float64(123.45),
@@ -104,7 +103,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Bulk Error Message",
+			name:  "Bulk Error Message",
 			input: "!21\r\nSYNTAX invalid syntax\r\n",
 			expected: []interface{}{
 				fmt.Errorf("SYNTAX invalid syntax"),
@@ -112,7 +111,7 @@ func TestParse(t *testing.T){
 			expectedError: false,
 		},
 		{
-			name: "Map with Key-Value Pairs",
+			name:  "Map with Key-Value Pairs",
 			input: "%2\r\n+first\r\n:1\r\n+second\r\n:2\r\n",
 			expected: []interface{}{
 				map[interface{}]interface{}{
@@ -128,10 +127,10 @@ func TestParse(t *testing.T){
 		t.Run(test.name, func(t *testing.T) {
 			result, err := Parser(test.input)
 			if test.expectedError {
-				assert.Error(t,err)
+				assert.Error(t, err)
 			}
 
-			assert.Equal(t,test.expected,result)
+			assert.Equal(t, test.expected, result)
 		})
 	}
 }
